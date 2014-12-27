ccff8a73649582c3a89119d0df0794c872cc2a90
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2014-12-27 18:26:33 +1100

    Fixup location of Paste ini file for OpenShift.

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+import os
+
 import mod_wsgi.openshift
 
-mod_wsgi.openshift.start("--application-type", "paste",
-    "/app/kallithea-runtime/production.ini")
+OPENSHIFT_DATA_DIR = os.environ['OPENSHIFT_DATA_DIR']
+KALLITHEA_HOMEDIR = os.path.join(OPENSHIFT_DATA_DIR, 'kallithea-runtime')
+KALLITHEA_INIFILE = os.path.join(KALLITHEA_HOMEDIR, 'production.ini')
+
+mod_wsgi.openshift.start("--application-type", "paste", KALLITHEA_INIFILE)

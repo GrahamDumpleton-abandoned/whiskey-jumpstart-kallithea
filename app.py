b3db7c187c29c53b1d09fb8fec9aaab651436866
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2014-12-27 21:05:14 +1100

    Increase capacity to combat long average response times.

--- app.py
+++ app.py
@@ -6,4 +6,14 @@ OPENSHIFT_DATA_DIR = os.environ['OPENSHIFT_DATA_DIR']
 KALLITHEA_HOMEDIR = os.path.join(OPENSHIFT_DATA_DIR, 'kallithea-runtime')
 KALLITHEA_INIFILE = os.path.join(KALLITHEA_HOMEDIR, 'production.ini')
 
-mod_wsgi.openshift.start("--application-type", "paste", KALLITHEA_INIFILE)
+# Note that because average response time is quite long, we increase the
+# capacity for concurrent requests. Each Kallithea instance is quite
+# memory hungry though so although increasing processes over threads can
+# be better, we can't set it too high else we will run out of memory with
+# a small gear. Most requests will be I/O bound on accessing database or
+# waiting on git commands, we use of many threads in one process shouldn't
+# be an issue. As a guard against memory increasing over time, if the
+# processes are idle for 5 minutes, then also attempt a restart.
+
+mod_wsgi.openshift.start('--application-type', 'paste', KALLITHEA_INIFILE,
+        '--processes', '2', '--threads', '15')
